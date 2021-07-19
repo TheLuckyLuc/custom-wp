099bdab155bb2def7f8bee0e8ceff8bcd23ede3a
Luc Berger <luc.berger@brace.co.uk>
2021-07-19 12:09:40 +0100

    Update all npm packages to latest versions

--- template-parts/none.php
+++ template-parts/none.php
@@ -1,4 +1,4 @@
-<section class="py-20 px-1/10 text-center">
+<section class="py-20 px-4 text-center">
 	<h1 class="text-2xl xs:text-3xl"><?php esc_html_e('Nothing Found'); ?></h1>
 	<p class="text-1xl xs:text-2xl"><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for.' ); ?></p>
 </section>
